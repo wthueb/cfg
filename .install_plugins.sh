ccd32a0d131446d56acfd0cc6ad3b23af9534f8b
William Huebner <wilhueb@gmail.com>
2019-02-27 02:14:25 -0500

    ycm doesn't fully install now

--- .install_plugins.sh
+++ .install_plugins.sh
@@ -2,6 +2,9 @@
 
 vim +PluginInstall +qall
 
-cd $HOME/.vim/bundle/YouCompleteMe/
-
-python3 install.py --clang-completer
+if [ $# -eq 0 ]; then
+    echo 'run with an argument to fully install youcompleteme'
+else
+    cd $HOME/.vim/bundle/YouCompleteMe/
+    python3 install.py --clang-completer
+fi
