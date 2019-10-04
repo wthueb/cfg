7d33e9dcac2e66f1328308b7f7e5598b552a88f8
William Huebner <wilhueb@gmail.com>
2019-10-04 00:21:07 -0400

    hopefully maybe one day we will have our ycm settings perfected

--- .vim/ycm_extra_conf.py
+++ .vim/ycm_extra_conf.py
@@ -2,5 +2,9 @@ def Settings(**kwargs):
     client_data = kwargs['client_data']
 
     return {
-        'interpreter_path': client_data['g:ycm_python_interpreter_path']
+        'interpreter_path': client_data['g:ycm_python_interpreter_path'],
+
+        # -I../include works assume the header files
+        # are in proj/include/ and the source file is in proj/*/
+        'flags': ['-x', 'c++', '-Wall', '-Wextra', '-std=c++14', '-I../include']
     }
