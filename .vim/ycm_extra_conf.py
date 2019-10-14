679548711b62b7438503d93a96715c18fb39b49d
William Huebner <wilhueb@gmail.com>
2019-10-13 20:50:28 -0400

    youcompleteme stuff

--- .vim/ycm_extra_conf.py
+++ .vim/ycm_extra_conf.py
@@ -1,10 +1,32 @@
+from os import environ
+import os.path
+
 def Settings(**kwargs):
     client_data = kwargs['client_data']
 
-    return {
-        'interpreter_path': client_data['g:ycm_python_interpreter_path'],
+    path = None
 
-        # -I../include works assume the header files
-        # are in proj/include/ and the source file is in proj/*/
+    if 'PATH' in environ:
+        for p in environ['PATH'].split(':'):
+            potential = os.path.join(p, 'python')
+
+            if os.path.exists(potential):
+                path = potential
+                break
+
+    if not path:
+        if os.path.exists('env/bin'):
+            path = 'env/bin/python'
+        else:
+            path = '/usr/local/bin/python'
+
+    return {
+        # python interpreter path for python syntax highlighting and completion
+        'interpreter_path': path,
+
+        # gcc flags for c syntax highlighting and completion
+
+        # -I../include works assuming the header files are
+        # in PRJ_ROOT/include/ and the source files are in PRJ_ROOT/src/
         'flags': ['-x', 'c++', '-Wall', '-Wextra', '-std=c++14', '-I../include']
     }
