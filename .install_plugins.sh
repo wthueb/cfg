e38cc36582d62151cf3e2f2004d7a55cb4f5d64f
William Huebner <wilhueb@gmail.com>
2019-10-30 04:13:23 -0400

    all of the completers pls

--- .install_plugins.sh
+++ .install_plugins.sh
@@ -2,9 +2,10 @@
 
 vim +PluginInstall +qall
 
-if [ $# -eq 0 ]; then
+if [[ $# == 0 ]]; then
     echo 'run with an argument to fully install youcompleteme'
 else
-    cd $HOME/.vim/bundle/YouCompleteMe/
-    python3 install.py --clang-completer
+    cd ~/.vim/bundle/YouCompleteMe/
+
+    python3 install.py --clang-completer --ts-completer --java-completer
 fi
