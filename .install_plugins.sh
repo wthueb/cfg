f868197a778aeb8814c7ada8763575a14803ef95
William Huebner <wilhueb@gmail.com>
2020-02-28 21:58:46 -0500

    notify that cmake and npm need to be installed to install youcompleteme

--- .install_plugins.sh
+++ .install_plugins.sh
@@ -5,6 +5,16 @@ vim +PluginInstall +qall
 if [[ $# == 0 ]]; then
     echo 'run with an argument to fully install youcompleteme'
 else
+    if [[ $(which cmake) == '' ]]; then
+        echo 'cmake not installed'
+        return
+    fi
+
+    if [[ $(which npm) == '' ]]; then
+        echo 'npm not installed'
+        return
+    fi
+
     cd ~/.vim/bundle/YouCompleteMe/
 
     python3 install.py --clang-completer --ts-completer --java-completer
