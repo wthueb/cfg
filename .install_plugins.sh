1d7c338f80f6b9fdd7424f2362b96dabbd1a9c4f
William Huebner <wilhueb@gmail.com>
2020-07-22 00:14:29 -0400

    coc completion instead of ycm

--- .install_plugins.sh
+++ .install_plugins.sh
@@ -1,50 +1,21 @@
 #!/usr/bin/env bash
 
+# install vim-plug
 curl -fLso ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-setting="export YCM_ENABLE="
-
-if [[ $YCM_ENABLE == "" ]]; then
-    if ! grep 'YCM_ENABLE' ~/.customprofile &> /dev/null; then
-        read -r -p "do you want to install youcompleteme completers? [y/n]: " response
-
-        if [[ -f ~/.customprofile ]]; then
-            echo >> ~/.customprofile
-        fi
-
-        if [[ ${response,,} =~ ^(yes|y)$ ]]; then
-            setting="${setting}1"
-
-            export YCM_ENABLE=1
-        else
-            setting="${setting}0"
-        fi
-
-        setting=$setting
-
-        echo $setting >> ~/.customprofile
-    fi
-fi
-
-if [[ $YCM_ENABLE ]]; then
-    if [[ $(which cmake) == '' ]]; then
-        echo 'cmake not installed'
-        exit
-    fi
-
-    if [[ $(which npm) == '' ]]; then
-        echo 'npm not installed'
-        exit
-    fi
-fi
 
 vim +PlugInstall +qall
 
 ln -s $HOME/.vim/bundle/vimpager/vimpager $HOME/.local/bin/ 2>/dev/null
 ln -s $HOME/.vim/bundle/vimpager/vimcat $HOME/.local/bin/ 2>/dev/null
 
-if [[ ${setting: -1} != "=" ]]; then
-    echo "added '$setting' to your ~/.customprofile"
+if [[ $(which npm) == '' ]]; then
+    echo 'npm not installed'
+    echo 'needed for coc completion'
+    exit 1
 fi
+
+cd ~/.config/coc/extensions/
+
+npm install
 
 exit 0
--- .vim/plugins.vim
+++ .vim/plugins.vim
@@ -7,6 +7,7 @@ Plug 'elzr/vim-json' " json syntax
 Plug 'godlygeek/tabular' " aligning text; required for vim-markdown
 Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --bin --completion --key-bindings --no-update-rc' } " fuzzy file finder
 Plug 'leafgarland/typescript-vim' " typescript syntax
+Plug 'neoclide/coc.nvim', { 'branch': 'release' } " autocompletion and stuff
 Plug 'nvie/vim-flake8' " flake8!
 Plug 'plasticboy/vim-markdown' " markdown syntax
 Plug 'preservim/nerdcommenter' " better commenting
@@ -15,13 +16,5 @@ Plug 'scrooloose/nerdtree' " file browser
 Plug 'tpope/vim-fugitive' " git
 Plug 'tpope/vim-surround' " change quotes and stuff
 Plug 'vim-python/python-syntax' " better python syntax
-
-let args = ''
-
-if $YCM_ENABLE == '1'
-    let args = ' --clang-completer --ts-completer --java-completer'
-endif
-
-Plug 'Valloric/YouCompleteMe', { 'do': 'python3 install.py' . args }
 
 call plug#end()
