25798e0a07e257d23310866c49d19a14cbb03d00
William Huebner <wilhueb@gmail.com>
2020-07-21 21:56:55 -0400

    better installation of plugins

--- .install_plugins.sh
+++ .install_plugins.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLso ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-if [[ $(which cmake) == '' ]]; then
-    echo 'cmake not installed'
-    exit
-fi
+read -r -p "do you want to install youcompleteme completers? [y/n]: " response
 
-if [[ $(which npm) == '' ]]; then
-    echo 'npm not installed'
-    exit
+[[ ${response,,} =~ ^(yes|y)$ ]] && export YCM_ENABLE=1
+
+if [[ $YCM_ENABLE ]]; then
+    if [[ $(which cmake) == '' ]]; then
+        echo 'cmake not installed'
+        exit
+    fi
+
+    if [[ $(which npm) == '' ]]; then
+        echo 'npm not installed'
+        exit
+    fi
 fi
 
 vim +PlugInstall +qall
 
-ln -s $HOME/.vim/bundle/vimpager/vimpager $HOME/.local/bin/
-ln -s $HOME/.vim/bundle/vimpager/vimcat $HOME/.local/bin/
+ln -s $HOME/.vim/bundle/vimpager/vimpager $HOME/.local/bin/ 2>/dev/null
+ln -s $HOME/.vim/bundle/vimpager/vimcat $HOME/.local/bin/ 2>/dev/null
+
+[[ $YCM_ENABLE ]] && echo "add 'export YCM_ENABLE=1' to your .customprofile"
+
+exit 0
--- .vim/plugins.vim
+++ .vim/plugins.vim
@@ -14,7 +14,12 @@ Plug 'rkitover/vimpager' " pager
 Plug 'scrooloose/nerdtree' " file browser
 Plug 'tpope/vim-fugitive' " git
 Plug 'tpope/vim-surround' " change quotes and stuff
-Plug 'Valloric/YouCompleteMe', { 'do': 'python3 install.py --clang-completer --ts-completer --java-completer' } " auto completion and syntax checking
 Plug 'vim-python/python-syntax' " better python syntax
+
+if !empty($YCM_ENABLE)
+    Plug 'Valloric/YouCompleteMe', { 'do': 'python3 install.py --clang-completer --ts-completer --java-completer' }
+else
+    Plug 'Valloric/YouCompleteMe', { 'do': 'python3 install.py' }
+endif
 
 call plug#end()
