27e5f8bd33de18aa839d9610fc556dba8ca4146c
William Huebner <wilhueb@gmail.com>
2020-07-21 22:31:45 -0400

    this makes sense right

--- .install_plugins.sh
+++ .install_plugins.sh
@@ -1,10 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 curl -fLso ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-read -r -p "do you want to install youcompleteme completers? [y/n]: " response
+setting="export YCM_ENABLE="
 
-[[ ${response,,} =~ ^(yes|y)$ ]] && export YCM_ENABLE=1
+if [[ $YCM_ENABLE == "" ]]; then
+    read -r -p "do you want to install youcompleteme completers? [y/n]: " response
+
+    if [[ -f ~/.customprofile ]]; then
+        echo >> ~/.customprofile
+    fi
+
+    if [[ ${response,,} =~ ^(yes|y)$ ]]; then
+        setting="${setting}1"
+
+        export YCM_ENABLE=1
+    else
+        setting="${setting}0"
+    fi
+
+    setting=$setting
+
+    echo $setting >> ~/.customprofile
+fi
 
 if [[ $YCM_ENABLE ]]; then
     if [[ $(which cmake) == '' ]]; then
@@ -23,6 +41,9 @@ vim +PlugInstall +qall
 ln -s $HOME/.vim/bundle/vimpager/vimpager $HOME/.local/bin/ 2>/dev/null
 ln -s $HOME/.vim/bundle/vimpager/vimcat $HOME/.local/bin/ 2>/dev/null
 
-[[ $YCM_ENABLE ]] && echo "add 'export YCM_ENABLE=1' to your .customprofile"
+if [[ ${setting: -1} != "=" ]]; then
+    echo "added '$setting' to your ~/.customprofile"
+    echo "you should probably source it"
+fi
 
 exit 0
