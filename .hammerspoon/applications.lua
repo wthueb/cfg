1146ef2dc0d8f72d399e4df30b2d0d582aef6139
William Huebner <wilhueb@gmail.com>
2021-02-08 12:15:43 -0500

    better window filter initialization

--- .hammerspoon/applications.lua
+++ .hammerspoon/applications.lua
@@ -1,6 +1,6 @@
 logger.i('loading applications')
 
-local finder = hs.window.filter.new(false):allowApp('Finder')
+local finder = hs.window.filter.new('Finder')
 
 enable_hk_for_wf(finder, hs.hotkey.new({}, 'delete', function()
     hs.application.get('Finder'):selectMenuItem({'File', 'Move to Trash'})
