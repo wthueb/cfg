c2500ed76d60ddb54e298ed3e82e78f928da3dd3
William Huebner <wilhueb@gmail.com>
2021-02-17 23:04:07 -0500

    fix arrows

--- .hammerspoon/arrows.lua
+++ .hammerspoon/arrows.lua
@@ -1,12 +1,9 @@
 -- use control + h/j/k/l for left/down/up/right
 logger.i('loading arrows')
 
-local not_iterm = hs.window.filter.new(true):rejectApp('iTerm2')
-
 function remap(from_mods, from_key, mods, key)
-    enable_hk_for_wf(not_iterm, hs.hotkey.new(from_mods, from_key, function()
-        key_press(mods, key)
-    end, nil, fn))
+    local fn = function() key_press(mods, key) end
+    hs.hotkey.bind(from_mods, from_key, fn, nil, fn)
 end
 
 remap({'ctrl'}, 'h', {}, 'left')
