cbe7a591712bd7ccd60a4d5e7f186dd60a2a3c98
William Huebner <wilhueb@gmail.com>
2023-09-01 14:53:53 -0400

    Windows stuff

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,23 @@
+Set-Alias cl clear
+
 function .. { cd .. }
+
 Set-Alias l ls
 function l { ls $args }
 function ll { ls -l $args }
 
+Set-Alias cat bat
+Set-Alias p python
 Set-Alias vim nvim
+
+function config { git --git-dir=$HOME\.cfg --work-tree=$HOME $args }
 
 Import-Module PSReadLine
 Set-PSReadLineOption -EditMode vi
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd:$true
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+Import-Module posh-git
