9a5d5ca67edb21b458a438d077700c869e4eea12
William Huebner <wilhueb@gmail.com>
2023-09-13 00:17:41 -0400

    Ignore .git/ with telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,6 +13,7 @@ return {
                         "fd",
                         "--type", "f",
                         "--hidden",
+                        "--exclude", ".git",
                     }
                 }
             }
