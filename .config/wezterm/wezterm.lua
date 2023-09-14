5077a5a9a617acc25e7c36de61b0c69b5cafeec1
William Huebner <wilhueb@gmail.com>
2023-09-13 21:50:39 -0400

    wezterm=tmux only on windows

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,33 +4,11 @@ local config = {
     color_scheme = "nord",
 
     font = wezterm.font "SauceCodePro Nerd Font",
-    font_size = 14.0,
+    font_size = 15.0,
 
-    use_fancy_tab_bar = false,
-    tab_bar_at_bottom = true,
-    tab_max_width = 18,
+    disable_default_key_bindings = true,
 
     audible_bell = "Disabled",
-
-    leader = { mods = "CTRL", key = "a" },
-    disable_default_key_bindings = true,
-    keys = {
-        { key = "a",   mods = "LEADER|CTRL",  action = wezterm.action.SendString("\x01") },
-        { key = "c",   mods = "LEADER",       action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-        { key = "1",   mods = "LEADER",       action = wezterm.action.ActivateTab(0) },
-        { key = "2",   mods = "LEADER",       action = wezterm.action.ActivateTab(1) },
-        { key = "3",   mods = "LEADER",       action = wezterm.action.ActivateTab(2) },
-        { key = "4",   mods = "LEADER",       action = wezterm.action.ActivateTab(3) },
-        { key = "5",   mods = "LEADER",       action = wezterm.action.ActivateTab(4) },
-        { key = "6",   mods = "LEADER",       action = wezterm.action.ActivateTab(5) },
-        { key = "7",   mods = "LEADER",       action = wezterm.action.ActivateTab(6) },
-        { key = "8",   mods = "LEADER",       action = wezterm.action.ActivateTab(7) },
-        { key = "9",   mods = "LEADER",       action = wezterm.action.ActivateTab(8) },
-        { key = "a",   mods = "LEADER",       action = wezterm.action.ActivateLastTab },
-        { key = "&",   mods = "LEADER|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
-        { key = "Tab", mods = "CTRL",         action = wezterm.action.ActivateTabRelative(1) },
-        { key = "Tab", mods = "CTRL|SHIFT",   action = wezterm.action.ActivateTabRelative(-1) },
-    },
 
     window_padding = {
         top = 1,
@@ -40,6 +18,13 @@ local config = {
     },
 
     launch_menu = {},
+
+    skip_close_confirmation_for_processes_named = {},
+
+    use_fancy_tab_bar = false,
+    tab_bar_at_bottom = true,
+    tab_max_width = 18,
+    hide_tab_bar_if_only_one_tab = true,
 
     colors = {
         tab_bar = {
@@ -71,14 +56,33 @@ local config = {
 local platform_keys = {}
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    config.default_prog = { "pwsh.exe" }
+
     table.insert(config.launch_menu, {
         label = "PowerShell",
         args = { "pwsh.exe" },
     })
 
-    config.default_prog = { "pwsh.exe" }
+    config.hide_tab_bar_if_only_one_tab = false
+
+    config.leader = { mods = "CTRL", key = "a", timeout_milliseconds = 5000 }
 
     platform_keys = {
+        { key = "a",   mods = "LEADER|CTRL",  action = wezterm.action.SendString("\x01") },
+        { key = "c",   mods = "LEADER",       action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+        { key = "1",   mods = "LEADER",       action = wezterm.action.ActivateTab(0) },
+        { key = "2",   mods = "LEADER",       action = wezterm.action.ActivateTab(1) },
+        { key = "3",   mods = "LEADER",       action = wezterm.action.ActivateTab(2) },
+        { key = "4",   mods = "LEADER",       action = wezterm.action.ActivateTab(3) },
+        { key = "5",   mods = "LEADER",       action = wezterm.action.ActivateTab(4) },
+        { key = "6",   mods = "LEADER",       action = wezterm.action.ActivateTab(5) },
+        { key = "7",   mods = "LEADER",       action = wezterm.action.ActivateTab(6) },
+        { key = "8",   mods = "LEADER",       action = wezterm.action.ActivateTab(7) },
+        { key = "9",   mods = "LEADER",       action = wezterm.action.ActivateTab(8) },
+        { key = "a",   mods = "LEADER",       action = wezterm.action.ActivateLastTab },
+        { key = "&",   mods = "LEADER|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
+        { key = "Tab", mods = "CTRL",         action = wezterm.action.ActivateTabRelative(1) },
+        { key = "Tab", mods = "CTRL|SHIFT",   action = wezterm.action.ActivateTabRelative(-1) },
         {
             mods = "CTRL|SHIFT",
             key = "t",
@@ -87,12 +91,7 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
         {
             mods = "CTRL|SHIFT",
             key = "w",
-            action = wezterm.action.CloseCurrentTab({ confirm = true }),
-        },
-        {
-            mods = "CTRL|SHIFT",
-            key = "q",
-            action = wezterm.action.QuitApplication,
+            action = wezterm.action.CloseCurrentTab({ confirm = false }),
         },
         {
             mods = "CTRL|SHIFT",
@@ -120,7 +119,7 @@ elseif wezterm.target_triple == "aarch64-apple-darwin" then
         {
             mods = "CMD",
             key = "w",
-            action = wezterm.action.CloseCurrentTab({ confirm = true }),
+            action = wezterm.action.CloseCurrentTab({ confirm = false }),
         },
         {
             mods = "CMD",
@@ -146,6 +145,10 @@ elseif wezterm.target_triple == "aarch64-apple-darwin" then
 end
 
 for _, key_config in pairs(platform_keys) do
+    if (config.keys == nil) then
+        config.keys = {}
+    end
+
     table.insert(config.keys, key_config)
 end
 
