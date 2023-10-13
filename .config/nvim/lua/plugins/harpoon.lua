8e202c66d0d434a069fa925a2f6d0c328dcbf58b
William Huebner <wilhueb@gmail.com>
2023-10-13 01:56:47 -0400

    Use space as leader

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -6,7 +6,7 @@ return {
         local mark = require("harpoon.mark")
         local ui = require("harpoon.ui")
 
-        vim.keymap.set("n", "<C-a>", mark.add_file, { silent = true, desc = "Add harpoon mark" })
+        vim.keymap.set("n", "<leader>a", mark.add_file, { silent = true, desc = "Add harpoon mark" })
         vim.keymap.set("n", "<C-s>", ui.toggle_quick_menu, { silent = true, desc = "Toggle harpoon quick menu" })
         vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end, { silent = true, desc = "Go to harpoon #1" })
         vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end, { silent = true, desc = "Go to harpoon #2" })
