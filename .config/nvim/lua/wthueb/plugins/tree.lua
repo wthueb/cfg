530900c234915e1690effbc25714f6b5e5e76c59
William Huebner <wilhueb@gmail.com>
2024-05-28 00:59:29 -0400

    oil instead of nvim-tree

--- .config/nvim/lua/wthueb/plugins/tree.lua
+++ .config/nvim/lua/wthueb/plugins/tree.lua
@@ -1,14 +1,14 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
+    -- "nvim-tree/nvim-tree.lua",
+    -- version = "*",
+    -- lazy = false,
+    -- dependencies = "nvim-tree/nvim-web-devicons",
+    -- config = function()
+    --     vim.g.loaded_netrw = 1
+    --     vim.g.loaded_netrwPlugin = 1
 
-        require("nvim-tree").setup({})
+    --     require("nvim-tree").setup({})
 
-        vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<CR>", { silent = true, desc = "Toggle tree" })
-    end,
+    --     vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<CR>", { silent = true, desc = "Toggle tree" })
+    -- end,
 }
