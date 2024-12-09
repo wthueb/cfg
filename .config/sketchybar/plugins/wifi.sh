1bdfa6d016c534019295c8f671ef337320c18a4b
William Huebner <wilhueb@gmail.com>
2024-12-08 22:59:14 -0500

    use ipconfig to get ssid instead of networksetup

--- .config/sketchybar/plugins/wifi.sh
+++ .config/sketchybar/plugins/wifi.sh
@@ -8,7 +8,7 @@ update() {
   local has_ip
 
   adapter=$(networksetup -listallhardwareports | awk '/Wi-Fi/ { getline; print $2 }')
-  ssid=$(networksetup -getairportnetwork en0 | awk -F ': ' '{ print $2 }')
+  ssid=$(ipconfig getsummary en0 | awk -F ' SSID : '  '/ SSID : / {print $2}')
   ip=$(ipconfig getifaddr "$adapter")
   has_ip=$?
 
