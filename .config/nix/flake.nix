ae07d3d6b56b48db54ae93c4a5bc22638fc3745f
William Huebner <wilhueb@gmail.com>
2024-12-16 19:37:45 -0500

    upgrade nixpkgs-stable

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
 
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
