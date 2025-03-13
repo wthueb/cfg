774591336f21fd6886599379a82f92b07307672e
William Huebner <whuebner@cleverdevices.com>
2025-03-13 11:53:07 -0400

    starship in powershell

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -20,8 +20,6 @@ Set-PSReadLineKeyHandler -Key Tab -Function Complete
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-Import-Module posh-git
-
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
@@ -31,6 +29,8 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+Invoke-Expression (&starship init powershell)
 
 if (test-path $env:USERPROFILE\.config\powershell\custom.ps1) {
     . $env:USERPROFILE\.config\powershell\custom.ps1
