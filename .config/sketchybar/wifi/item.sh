3411e8a6406cf444f0596f04745cbdb4ac88078a
William Huebner <wilhueb@gmail.com>
2025-12-30 04:46:50 -0500

    sketchybar wifi update on poll in case missed event on startup

--- .config/sketchybar/wifi/item.sh
+++ .config/sketchybar/wifi/item.sh
@@ -2,8 +2,15 @@
 
 source "$CONFIG_DIR/icons.sh"
 
+wifi=(
+  script=wifi/script.sh
+  icon="$WIFI_DISCONNECTED"
+  label.width=0
+  update_freq=60
+)
+
 sketchybar --add item wifi right \
-           --set wifi script=wifi/script.sh icon="$WIFI_DISCONNECTED" label.width=0 \
+           --set wifi "${wifi[@]}" \
            --subscribe wifi wifi_change mouse.entered mouse.exited
 
 sketchybar --add item wifi.info popup.wifi
