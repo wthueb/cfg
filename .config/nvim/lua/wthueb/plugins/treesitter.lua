0ca103166d202ae9e0f994d8d10a7af383f7739f
William Huebner <wilhueb@gmail.com>
2025-12-31 03:07:23 -0500

    fix treesitter config

--- .config/nvim/lua/wthueb/plugins/treesitter.lua
+++ .config/nvim/lua/wthueb/plugins/treesitter.lua
@@ -4,73 +4,75 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
-    ---@type TSConfig
-    ---@diagnostic disable-next-line: missing-fields
-    opts = {
-        ensure_installed = {
-            "angular",
-            "bash",
-            "c",
-            "c_sharp",
-            "cpp",
-            "css",
-            "html",
-            "javascript",
-            "lua",
-            "nu",
-            "python",
-            "rust",
-            "scss",
-            "sql",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "query",
-        },
-
-        sync_install = false,
-        auto_install = true,
-
-        highlight = { enable = true },
-
-        indent = { enable = true },
-
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<C-space>",
-                node_incremental = "<C-space>",
-                scope_incremental = "<C-s>",
-                node_decremental = "<C-b>",
+    config = function()
+        ---@diagnostic disable-next-line: missing-fields
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "angular",
+                "bash",
+                "c",
+                "c_sharp",
+                "cpp",
+                "css",
+                "html",
+                "javascript",
+                "lua",
+                "nu",
+                "python",
+                "rust",
+                "scss",
+                "sql",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "query",
             },
-        },
 
-        textobjects = {
-            move = {
+            sync_install = false,
+            auto_install = true,
+
+            highlight = {
                 enable = true,
-                set_jumps = true,
-                goto_next_start = {
-                    ["]m"] = "@function.outer",
-                    ["]]"] = "@class.outer",
-                    ["]o"] = "@loop.*",
-                    ["]s"] = "@scope",
-                    ["]z"] = "@fold",
-                },
-                goto_next_end = {
-                    ["]M"] = "@function.outer",
-                    ["]["] = "@class.outer",
-                },
-                goto_previous_start = {
-                    ["[m"] = "@function.outer",
-                    ["[["] = "@class.outer",
-                },
-                goto_previous_end = {
-                    ["[M"] = "@function.outer",
-                    ["[]"] = "@class.outer",
+                additional_vim_regex_highlighting = false,
+            },
+
+            indent = { enable = true },
+
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = "<C-s>",
+                    node_decremental = "<C-b>",
                 },
             },
-        },
 
-        additional_vim_regex_highlighting = false,
-    },
+            textobjects = {
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["]m"] = "@function.outer",
+                        ["]]"] = "@class.outer",
+                        ["]o"] = "@loop.*",
+                        ["]s"] = "@scope",
+                        ["]z"] = "@fold",
+                    },
+                    goto_next_end = {
+                        ["]M"] = "@function.outer",
+                        ["]["] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["[m"] = "@function.outer",
+                        ["[["] = "@class.outer",
+                    },
+                    goto_previous_end = {
+                        ["[M"] = "@function.outer",
+                        ["[]"] = "@class.outer",
+                    },
+                },
+            },
+        })
+    end,
 }
