326b6021b7014f06810d02021cf0a9b483f3532e
William Huebner <wilhueb@gmail.com>
2026-01-17 23:39:59 -0500

    undotree -> atone.nvim

---
+++ .config/nvim/lua/wthueb/plugins/atone.lua
@@ -0,0 +1,10 @@
+---@module "lazy"
+---@type LazySpec
+return {
+    "XXiaoA/atone.nvim",
+    opts = {},
+    cmd = "Atone",
+    keys = {
+        { "<leader>u", "<cmd>Atone<CR>", desc = "Toggle undotree" },
+    },
+}
--- .config/nvim/lua/wthueb/plugins/undotree.lua
+++
@@ -1,8 +0,0 @@
----@module "lazy"
----@type LazySpec
-return {
-    "mbbill/undotree",
-    config = function()
-        vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<CR>", { silent = true, desc = "Toggle undotree" })
-    end,
-}
