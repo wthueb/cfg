d1de32a9fcc6d83a5ded0edc5664c833045ffa31
William Huebner <wilhueb@gmail.com>
2026-01-29 00:03:39 -0500

    use drake as build machine instead of mbk

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -5,13 +5,21 @@
   determinateNix = {
     enable = true;
     buildMachines = [
+      # {
+      #   hostName = "mbk";
+      #   protocol = "ssh-ng";
+      #   sshUser = "wil";
+      #   sshKey = "/Users/wil/.ssh/id_ed25519";
+      #   systems = [ "x86_64-linux" ];
+      #   maxJobs = 4;
+      # }
       {
-        hostName = "mbk";
+        hostName = "drake";
         protocol = "ssh-ng";
         sshUser = "wil";
         sshKey = "/Users/wil/.ssh/id_ed25519";
         systems = [ "x86_64-linux" ];
-        maxJobs = 4;
+        maxJobs = 2;
       }
     ];
     distributedBuilds = true;
