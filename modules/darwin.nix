ebdda9fde08450641c651f2f024d349d0617598e
William Huebner <wilhueb@gmail.com>
2026-01-29 00:13:41 -0500

    add supportedFeatures to build machines

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -7,19 +7,21 @@
     buildMachines = [
       # {
       #   hostName = "mbk";
+      #   systems = [ "x86_64-linux" ];
+      #   supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      #   maxJobs = 4;
       #   protocol = "ssh-ng";
       #   sshUser = "wil";
       #   sshKey = "/Users/wil/.ssh/id_ed25519";
-      #   systems = [ "x86_64-linux" ];
-      #   maxJobs = 4;
       # }
       {
         hostName = "drake";
+        systems = [ "x86_64-linux" ];
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+        maxJobs = 2;
         protocol = "ssh-ng";
         sshUser = "wil";
         sshKey = "/Users/wil/.ssh/id_ed25519";
-        systems = [ "x86_64-linux" ];
-        maxJobs = 2;
       }
     ];
     distributedBuilds = true;
