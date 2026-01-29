65c48c473034a527c5826d0da45c367a8bb829f8
William Huebner <wilhueb@gmail.com>
2026-01-29 00:50:17 -0500

    telescope grep hidden files

--- dotfiles/.config/nvim/lua/wthueb/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/wthueb/plugins/telescope.lua
@@ -2,7 +2,7 @@
 ---@type LazySpec
 return {
     "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
+    version = "*",
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = vim.fn.executable("make") == 1 },
@@ -19,6 +19,16 @@ return {
                         ["<C-q>"] = require("telescope.actions").send_to_qflist,
                         ["<C-t>"] = require("trouble.sources.telescope").open,
                     },
+                },
+                vimgrep_arguments = {
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    "--hidden",
                 },
             },
             pickers = {
@@ -37,35 +47,47 @@ return {
 
         -- Enable telescope fzf native, if installed
         pcall(require("telescope").load_extension, "fzf")
-
-        vim.keymap.set(
-            "n",
+    end,
+    keys = {
+        {
             "<leader><space>",
             "<cmd>Telescope buffers<CR>",
-            { silent = true, desc = "Telescope open buffers" }
-        )
-        vim.keymap.set(
-            "n",
+            desc = "Telescope open buffers",
+        },
+        {
             "<leader>/",
             "<cmd>Telescope current_buffer_fuzzy_find<CR>",
-            { silent = true, desc = "Telescope in current buffer" }
-        )
-        vim.keymap.set("n", "<leader>sr", "<cmd>Telescope oldfiles<CR>", { silent = true, desc = "Recently opened" })
-        vim.keymap.set(
-            "n",
+            desc = "Telescope in current buffer",
+        },
+        {
             "<leader>sf",
-            "<cmd>Telescope find_files hidden=true<CR>",
-            { silent = true, desc = "Files" }
-        )
-        vim.keymap.set(
-            "n",
+            "<cmd>Telescope find_files<CR>",
+            desc = "Files",
+        },
+        {
             "<leader>ss",
             "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>",
-            { silent = true, desc = "LSP symbols" }
-        )
-        vim.keymap.set("n", "<leader>sh", "<cmd>Telescope help_tags<CR>", { silent = true, desc = "Help" })
-        vim.keymap.set("n", "<leader>sg", "<cmd>Telescope live_grep<CR>", { silent = true, desc = "Grep" })
-        vim.keymap.set("n", "<leader>sd", "<cmd>Telescope diagnostics<CR>", { silent = true, desc = "Diagnostics" })
-        vim.keymap.set("n", "<C-p>", "<cmd>Telescope keymaps<CR>", { silent = true, desc = "Telescope keymaps" })
-    end,
+            desc = "LSP symbols",
+        },
+        {
+            "<leader>sh",
+            "<cmd>Telescope help_tags<CR>",
+            desc = "Help",
+        },
+        {
+            "<leader>sg",
+            "<cmd>Telescope live_grep<CR>",
+            desc = "Grep",
+        },
+        {
+            "<leader>sd",
+            "<cmd>Telescope diagnostics<CR>",
+            desc = "Diagnostics",
+        },
+        {
+            "<C-p>",
+            "<cmd>Telescope keymaps<CR>",
+            desc = "Telescope keymaps",
+        },
+    },
 }
