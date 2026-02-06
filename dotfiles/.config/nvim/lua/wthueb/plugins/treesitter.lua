303d20e24527cdaff03a06d4aa2bfae94275639c
William Huebner <wilhueb@gmail.com>
2026-02-06 02:52:08 -0500

    update treesitter to main branch

--- dotfiles/.config/nvim/lua/wthueb/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/wthueb/plugins/treesitter.lua
@@ -1,78 +1,39 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
+    -- TODO: nvim-treesitter/nvim-treesitter-textobjects
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
-    dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
     config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "angular",
-                "bash",
-                "c",
-                "c_sharp",
-                "cpp",
-                "css",
-                "html",
-                "javascript",
-                "lua",
-                "nu",
-                "python",
-                "rust",
-                "scss",
-                "sql",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "query",
-            },
+        local fts = {
+            "angular",
+            "bash",
+            "c",
+            "c_sharp",
+            "cpp",
+            "css",
+            "html",
+            "javascript",
+            "lua",
+            "nu",
+            "python",
+            "rust",
+            "scss",
+            "sql",
+            "typescript",
+            "vim",
+            "vimdoc",
+            "query",
+        }
 
-            sync_install = false,
-            auto_install = true,
+        require("nvim-treesitter").install(fts)
 
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-
-            indent = { enable = true },
-
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = "<C-s>",
-                    node_decremental = "<C-b>",
-                },
-            },
-
-            textobjects = {
-                move = {
-                    enable = true,
-                    set_jumps = true,
-                    goto_next_start = {
-                        ["]m"] = "@function.outer",
-                        ["]]"] = "@class.outer",
-                        ["]o"] = "@loop.*",
-                        ["]s"] = "@scope",
-                        ["]z"] = "@fold",
-                    },
-                    goto_next_end = {
-                        ["]M"] = "@function.outer",
-                        ["]["] = "@class.outer",
-                    },
-                    goto_previous_start = {
-                        ["[m"] = "@function.outer",
-                        ["[["] = "@class.outer",
-                    },
-                    goto_previous_end = {
-                        ["[M"] = "@function.outer",
-                        ["[]"] = "@class.outer",
-                    },
-                },
-            },
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = fts,
+            callback = function()
+                vim.treesitter.start()
+            end,
         })
     end,
 }
