3ecd1943e51239b3d95afb482f8e8508a8639210
William Huebner <wilhueb@gmail.com>
2026-08-21 11:07:38 -0400

    update flake.lock

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     };
 
     i915-sriov = {
-      url = "github:strongtz/i915-sriov-dkms?tag=2026.08.08";
+      url = "github:strongtz/i915-sriov-dkms/2026.08.12.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
